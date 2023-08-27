5b843594c0119c9707f3749419d212302f7a97d6
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-08-27 22:56:39 +0400

    Adding color variables/fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App()  {
 
 
   return (
-   <div className="w-screen h-screen bg-red-300"> 
-
+   <div className="w-screen h-screen bg-red-300 font-custom"> 
+      <h1 className="text-myColor">This is title</h1>
    </div> 
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['Space Mono', 'sans-serif']
+      },
+      colors: {
+          blueCommon: '#0079FF',
+          greyFirst: '#697C9A',
+          greySecond: '#F6F8FF',
+          darkBlueFirst: '#4B6A9B',
+          darkBlueSecond: '#1E2A47',
+          blackFirst: '#2B3442',
+          blackSecond: '#141D2F',
+          whiteFirst: '#FEFEFE',
+          whiteSecond: 'FFFFFF'
+      }
+    },
   },
   plugins: [],
 }
