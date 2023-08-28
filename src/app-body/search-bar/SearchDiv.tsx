7de684d71f312b215409026bda8e14829b2da85e
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-08-28 11:52:04 +0400

    Adding search bar/icon and some styles of search divison

--- src/app-body/search-bar/SearchDiv.tsx
+++ src/app-body/search-bar/SearchDiv.tsx
@@ -1,6 +1,10 @@
 import './SearchDiv.css';
 import React from 'react';
 
+import { SearchBtn  } from '../../components/main';
+
+import SearchLogo from '../../assets/search-icon.png';
+
 interface SearchDivProps{
 
 }
@@ -9,7 +13,55 @@ const SearchDiv: React.FC<SearchDivProps> = ({}) =>
 {
     return (
         <>
-        <div>Search Div!</div>       
+         <div className='
+          rounded-[15px]
+          bg-whiteFirst
+          shadow-md
+          max-w-[730px]
+          w-full
+          p-[9.5px]
+          px-[10px]
+
+          flex
+          justify-between
+          items-center
+         '>
+            <div className='
+             flex
+             items-center
+             justify-center
+             mx-6
+             space-x-6 
+             '>
+                <img
+                 src={SearchLogo}
+                 alt='Search loop' />
+                <input
+                 className='
+                  text-base
+                  text-darkBlueFirst
+                  font-normal
+                  leading-6
+                  min-w-[254px]
+                  border-none
+                  outline-none
+                 ' 
+                 placeholder='Search GitHub username…'/>
+            </div>
+            <div className='
+             flex
+             item-center
+             justify-center
+             space-x-6
+            '>
+              {/* <span className='
+               text-red-600
+               text-1g
+               font-semibold
+              '>No results</span> */}
+              <SearchBtn /> 
+            </div>
+         </div>       
         </>
     )
 }
